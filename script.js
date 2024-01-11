2ac77e89783bb1bbeb2c4b8791d15dd11ab5c23b
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-01-10 23:47:07 -0300

    adds clear functionality to x and Clear All btn

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear");
 
 function addItem(e) {
   e.preventDefault();
@@ -35,5 +36,19 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
 //Even Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
